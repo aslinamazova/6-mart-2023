c62b9dc9975ef9a621f414022e6cc74c663284b7
aslinamazova <76001yw@code.edu.az>
2023-03-06 03:21:35 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// tapsiriq1
+int DateOfBirthday = 2004;
+if (DateOfBirthday + 18 <= 2023)
+{
+    Console.WriteLine("18 yasiniz tamam olub");
+}
+else
+{
+    Console.WriteLine("18 yasiniz tamam olmayib");
+}
+
+//tapsiriq3
+int num= abcd
+    abcd=1234
+    int x=a+b+c+d
+
+    Console.WriteLine(x);
+
